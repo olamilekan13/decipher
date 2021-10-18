418299ab035ecae5e2af95c7f7d9185698a917bc
olamilekan <olamilekanomosanya@gmail.com>
2021-10-18 16:52:21 +0100

    add my first and second function inside another function

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ let userInput = prompt("Please User Enter a Sentence");
 let firstLetter = userInput.charAt(0).toUpperCase();
 let lastLetter = userInput.charAt(userInput.length - 1).toUpperCase();
 let fl = firstLetter.concat(lastLetter);
-document.write(`your input is ${userInput}`);
+document.write(`your input is : ${userInput}`);
 function firstAndLast(){
   let firstLetter = userInput.charAt(0).toUpperCase();
   let lastLetter = userInput.charAt(userInput.length - 1).toUpperCase();
   let fl = firstLetter.concat(lastLetter);
-  document.write(`<p>Your first letter is ${firstLetter} and your last letter is ${lastLetter}</p>`);
+  document.write(`<p>Your first letter is  :${firstLetter} and your last letter is :${lastLetter}</p>`);
 }
+
 firstAndLast();
 
 function rev(){
@@ -16,5 +17,19 @@ function rev(){
   return document.write(str);
 }
 
+//let reversal = rev();
+
 rev();
+
+function third(){
+ let reversal = rev();
+ let f_A_L = firstAndLast()
+
+}
+
+third();
+
+
+
+
 
