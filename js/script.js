45ad970bd000a48e0a4f2081caac50bf679fb100
olamilekan <olamilekanomosanya@gmail.com>
2021-10-18 15:11:39 +0100

    add function to reverse the first and last string

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 let userInput = prompt("Please User Enter a Sentence");
+let firstLetter = userInput.charAt(0).toUpperCase();
+let lastLetter = userInput.charAt(userInput.length - 1).toUpperCase();
+let fl = firstLetter.concat(lastLetter);
 document.write(`your input is ${userInput}`);
 function firstAndLast(){
-  let firstLetter = userInput.charAt(0);
-  let lastLetter = userInput.charAt(userInput.lenght - 1);
+  let firstLetter = userInput.charAt(0).toUpperCase();
+  let lastLetter = userInput.charAt(userInput.length - 1).toUpperCase();
+  let fl = firstLetter.concat(lastLetter);
+  document.write(`<p>Your first letter is ${firstLetter} and your last letter is ${lastLetter}</p>`);
 }
+firstAndLast();
+
+function rev(){
+  let str = fl.split('').reverse().join('');
+  return document.write(str);
+}
+
+rev();
+
