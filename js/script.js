83eb816685c940899cf4b4d215de1b14b6203cd5
olamilekan <olamilekanomosanya@gmail.com>
2021-10-18 10:29:04 +0100

    add function to take the first and last string and set it to uppercase

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 let userInput = prompt("Please User Enter a Sentence");
-document.write(`your input is ${userInput}`)
+document.write(`your input is ${userInput}`);
+function firstAndLast(){
+  let firstLetter = userInput.charAt(0);
+  let lastLetter = userInput.charAt(userInput.lenght - 1);
+}
