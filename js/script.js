8cf9d55bc22e51b6ff6a22e6853094e4c76565ad
olamilekan <olamilekanomosanya@gmail.com>
2021-10-19 20:14:18 +0100

    add my fourth function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
+// to prompt user to enter a sentence
 let userInput = prompt("Please User Enter a Sentence");
+// to take the first letter of the user input and capitalize it
 let firstLetter = userInput.charAt(0).toUpperCase();
+// to take the last letter of the user input and capitalize it
 let lastLetter = userInput.charAt(userInput.length - 1).toUpperCase();
+// concantenate my first letter and last letter
 let fl = firstLetter.concat(lastLetter);
+
 document.write(`your input is : ${userInput}`);
+
+//function to take the 1st and 2nd letter and concatenate
 function firstAndLast(){
-  let firstLetter = userInput.charAt(0).toUpperCase();
-  let lastLetter = userInput.charAt(userInput.length - 1).toUpperCase();
-  let fl = firstLetter.concat(lastLetter);
-  document.write(`<p>Your first letter is  :${firstLetter} and your last letter is :${lastLetter}</p>`);
+  fl ;
+  document.write(`<p>Your first and last letter are  :${fl} </p>`);
 }
 
+// invoking the function
 firstAndLast();
 
+
+//function to reverse a string
 function rev(){
   let str = fl.split('').reverse().join('');
   return document.write(str);
@@ -21,15 +29,24 @@ function rev(){
 
 rev();
 
+// add 1st and 2nd function inside another function
 function third(){
  let reversal = rev();
- let f_A_L = firstAndLast()
+ let f_A_L = firstAndLast();
 
 }
 
 third();
 
+//add function to add the original string with the 1st nd 2nd letter
 
+function fourth(){
+  document.write(userInput.concat(fl));
+}
 
-
+fourth();
+// function to count the characters in user input
+function count(){
+ 
+}
 
